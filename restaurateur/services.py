aa01b3e037dac23b75fafd19c16badb777d81479
Kirill Rasuli <rasulikv@gmail.com>
2021-05-16 15:39:55 +0300

    Rework empty restuarants list handling

--- restaurateur/services.py
+++ restaurateur/services.py
@@ -16,10 +16,8 @@ def get_restaurants(order):
         for menu_item in menu_items:
             restauraunts_for_item.add(menu_item.restaurant)
         restaurants_for_order.append(restauraunts_for_item)
-    common_restaurants_for_order = set.intersection(*restaurants_for_order)
-    if common_restaurants_for_order:
-        return common_restaurants_for_order
-    return ('Подходящих ресторанов нет!',)
+    return set.intersection(*restaurants_for_order)
+
 
 
 def fetch_coordinates(apikey, place):
